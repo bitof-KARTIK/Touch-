4c24ae54fa113a33f1dc4d5cccddedd9a287f429
Kartik Shah <157111737+bitof-KARTIK@users.noreply.github.com>
2024-07-13 01:52:24 +0530

    Update MobileNav.tsx

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -22,7 +22,7 @@ const MobileNav = () => {
                 </SheetTrigger>
                 <SheetContent side='left' className='border-none bg-dark-1'>
                     <Link href='/' className='flex items-center gap-1'>
-                        <Image src='/icons/logo.svg' width={32} height={32} alt='logo' className='max-sm:size-10' />
+                        <Image src='/icons/logo.png' width={32} height={32} alt='logo' className='max-sm:size-10' />
                         <p className='text-[26px] font-extrabold text-white'>Touch</p>
                     </Link>
                     <div className='flex h-[calc(100vh-72px)] flex-col justify-between overflow-y-auto'>
